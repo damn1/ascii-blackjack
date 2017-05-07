f7847b5d896b1291c9698efecfb59a885b0b281e
Damiano <damiano.enerli@mail.polimi.it>
2017-05-07 17:40:22 +0200

    Package re-organization.

--- d4mn-ascii-blackjack/src/D4mnAsciiBlackjack.java
+++ d4mn-ascii-blackjack/src/D4mnAsciiBlackjack.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package d4mn.ascii.blackjack;
+
 
 import static java.lang.Thread.sleep;
 import java.util.Scanner;
